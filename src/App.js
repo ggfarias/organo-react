4738aad16d7216c8b724d36541f212032306c19b
Gustavo Farias <gustavocgf@gmail.com>
2023-08-01 10:50:41 -0300

    Update Readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Banner from './components/Banner';
+import Formulario from './components/Formulario';
+import Time from './components/Time';
+
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Engenharia',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    }, 
+    {
+      nome: 'Infraestrutura',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8'
+    }, 
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFBA05',
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Produto',
+      corPrimaria: '#DB6EBF',
+      corSecundaria: '#FAE9F5'
+    }, 
+  ];
+
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    debugger
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria} 
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+      />)};
+    
     </div>
   );
-}
+};
 
 export default App;
